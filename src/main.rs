013588522efb31eb3c3c853f74792fe623b61c46
lycopersicum-t <anastasijanovik06@gmail.com>
2026-03-30 23:46:15 +0300

    proj struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use sea_orm::{Database, DatabaseConnection};
+
+pub struct AppState {
+    pub db: sea_orm::DatabaseConnection,
+}
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().ok();
+    let db_url = std::env::var("DATABASE_URL").expect("Set DATABASE_URL");
+    let db = Database::connect(db_url).await.expect("Failed to connect to the database");
+
 }
