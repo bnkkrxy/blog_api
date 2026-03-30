f2c3e751da79e1189cc8bc38d2ca326bc1d40859
lycopersicum-t <anastasijanovik06@gmail.com>
2026-03-30 22:39:42 +0300

    entities

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,6 +1,5 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_table;
 mod m20260330_183859_create_table_users;
 mod m20260330_183909_create_table_posts;
 
@@ -10,7 +9,6 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20260330_183859_create_table_users::Migration),
             Box::new(m20260330_183909_create_table_posts::Migration),
         ]
