ab10aa8c7fe8059cc6d6da567e404f331c6494aa
lycopersicum-t <anastasijanovik06@gmail.com>
2026-03-31 22:40:23 +0300

    errors

--- errors.rs
+++ errors.rs
@@ -1,10 +1,24 @@
-/*9. Обработка ошибок:
-- создать enum AppError,
-- маппить ошибки в HTTP-коды:
-404 — пользователь не найден,
-400 — невалидные данные,
-500 — внутренняя ошибка. */
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use serde_json::json;
 
 enum AppError {
-    
+    InvalidData(String), // 400
+    NotFound(String), // 404
+    InternalServer(String), // 500
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            InvalidData(err_msg) => (StatusCode::BAD_REQUEST, err_msg),
+            NotFound(err_msg) => (StatusCode::NOT_FOUND, err_msg),
+            InternalServer(err_msg) => (StatusCode::INTERNAL_SERVER_ERROR, err_msg),
+        };
+        let error = Json(json!({"error": error_message}));
+        (status, error).into_response()
+    }
 }
