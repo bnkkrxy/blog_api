ade89c71ec44806ed8e257078ef4b28b27de3dd0
lycopersicum-t <anastasijanovik06@gmail.com>
2026-04-01 21:09:43 +0300

    post_service()

--- src/services/post_service.rs
+++ src/services/post_service.rs
@@ -1,6 +1,6 @@
-use crate::repository::{self, post_repository, user_repository};
+use crate::repository::{post_repository, user_repository};
 use crate::errors::AppError;
-use crate::entities::posts;
+use crate::entities::{posts, users};
 use sea_orm::DatabaseConnection;
 
 pub async fn create_post(db: &DatabaseConnection, title: String, body: String, user_id: i32) -> Result<posts::Model, AppError> {
@@ -20,3 +20,20 @@ pub async fn create_post(db: &DatabaseConnection, title: String, body: String, u
         .map_err(|e| AppError::InternalServer(e.to_string()))
 }
 
+pub async fn get_all_posts(db: &DatabaseConnection) -> Result<Vec<posts::Model>, AppError> {
+    post_repository::find_all_posts(db)
+        .await
+        .map_err(|e| AppError::InternalServer(e.to_string()))
+}
+
+pub async fn get_posts_by_user_id(db: &DatabaseConnection, user_id: i32) -> Result<Vec<posts::Model>, AppError> {
+    post_repository::find_posts_by_user_id(db, user_id)
+        .await
+        .map_err(|e| AppError::InternalServer(e.to_string()))
+}
+
+pub async fn get_posts_with_authors(db: &DatabaseConnection) -> Result<Vec<(posts::Model, Option<users::Model>)>, AppError> {
+    post_repository::find_posts_with_authors(db)
+        .await
+        .map_err(|e| AppError::InternalServer(e.to_string()))
+}
